f9256e5d3d2457d8a887f36f03891918baa8cc36
Thomas Fouquert <tfouquert@gmail.com>
2022-11-11 13:23:55 +0100

    add data

---
+++ src/Data/footer.js
@@ -0,0 +1,21 @@
+export const footerData = [
+  {
+    id: 1,
+    language: 'React JS'
+  },
+  {
+    id: 2,
+    language: 'Reactb Bootstrap'
+  }
+]
+
+export const production = [
+  {
+    id: 1,
+    name: 'Heroku'
+  },
+  {
+    id: 2,
+    name: 'NameCheap'
+  }
+]
