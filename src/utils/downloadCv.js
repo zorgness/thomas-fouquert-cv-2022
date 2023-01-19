43e05218d98f3b2002b5e0df1087865258f677ca
Thomas Fouquert <tfouquert@gmail.com>
2023-01-19 17:08:21 +0100

    update

--- src/utils/downloadCv.js
+++ src/utils/downloadCv.js
@@ -1,13 +1,12 @@
 export const downloadCv = () => {
-  fetch('cv-thomas-fouquert.pdf').then(response => {
-    response.blob().then(blob => {
+  fetch("fouquert-thomas-2023-cv.pdf").then((response) => {
+    response.blob().then((blob) => {
+      const fileURL = window.URL.createObjectURL(blob);
 
-        const fileURL = window.URL.createObjectURL(blob);
-
-        let alink = document.createElement('a');
-        alink.href = fileURL;
-        alink.download = 'cv-thomas-fouquert.pdf';
-        alink.click();
-    })
-})
-}
+      let alink = document.createElement("a");
+      alink.href = fileURL;
+      alink.download = "cv-thomas-fouquert.pdf";
+      alink.click();
+    });
+  });
+};
