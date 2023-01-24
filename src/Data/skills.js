76285c54f331b4e2d81524de735f0dc2865f97de
Thomas Fouquert <tfouquert@gmail.com>
2023-01-24 12:02:51 +0100

    update

--- src/Data/skills.js
+++ src/Data/skills.js
@@ -4,24 +4,29 @@ export const skills = [
     name: "Github",
     description: "Github",
   },
-
   {
     id: 2,
+    name: "Trello",
+    description: "Trello, Backlog & Sprint",
+  },
+
+  {
+    id: 3,
     name: "Test",
     description: "React testing library",
   },
   {
-    id: 3,
+    id: 4,
     name: "English",
     description: "Anglais courant",
   },
   {
-    id: 4,
+    id: 5,
     name: "Docker",
     description: "Notions Docker",
   },
   {
-    id: 5,
+    id: 6,
     name: "Figma",
     description: "Figma, prototypage",
   },
